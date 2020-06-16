e8d301be20579c9932820e687330f7e84cb87c06
thiagoos16 <tos@icomp.ufam.edu.br>
2020-06-16 10:53:47 -0400

    add initial dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+
+
+const server =  http.listen(8080, function() {
+    console.log('Is running');
+});
